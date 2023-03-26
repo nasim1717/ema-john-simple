06727c6d4f1745e071a1c9a3f81ff9e54e3e12f7
nasim1717 <98159877+nasim1717@users.noreply.github.com>
2023-03-27 03:38:44 +0600

    product added

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Product.css';
+const Product = (props) => {
+    console.log(props);
+   const {img, name, seller, ratings, quantity, price} = props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+           <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacture: {seller}</p>
+                <p>Rating: {ratings}</p>
+           </div>
+           <button className='btn-cart'>Add to Cart</button>
+        </div>
+    );
+};
+export default Product;
