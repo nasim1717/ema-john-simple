ea92e38cbd2441e1960007a65e0f86d982964661
nasim1717 <98159877+nasim1717@users.noreply.github.com>
2023-03-28 04:33:29 +0600

    product quantity added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,18 +3,23 @@ import './Cart.css';
 const Cart = ({cart}) => {
     // const cart = props.cart; //oprion 1
     // const {cart} = props; //option 2
-    let totalPrice = 0, totalShiping = 0;
+    let totalPrice = 0, totalShiping = 0, quantity = 0;
     for(const product of cart){
-        totalPrice+= product.price;
+        if(product.quantity === 0){
+            product.quantity = 1;
+        }
+        // product.quantity = product.quantity || 1;
+        totalPrice+= product.price * product.quantity;
         totalShiping+= product.shipping;
+        quantity+= product.quantity; 
     }
     const tax = (totalPrice*7)/100;
     const grandTotal = totalPrice + totalShiping;
-
+    console.log(quantity);
     return (
         <div className='cart'>
             <h4>Order Summary </h4>
-            <p>Selected Items: {cart.length}</p> 
+            <p>Selected Items: {quantity}</p> 
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shiping: </p>
             <p>Tax: ${tax.toFixed(2)}</p>
