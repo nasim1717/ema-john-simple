36efe39267df48b6ccae3ee9440a8ff93f8f3927
nasim1717 <98159877+nasim1717@users.noreply.github.com>
2023-03-28 03:17:29 +0600

    retrieve product from stored cart and set it cart to set

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -13,17 +13,35 @@ const Shop = () => {
         .then(data => setProducts(data));
     }, []);
 
-    useEffect( ()=>{
-        const stroedCart = getShoppingCart();
-    }, []);
+
+    useEffect( () =>{
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // step 1: get id of the addedProduct
+        for(const id in storedCart){
+            //step 2: get product form products state by using id
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+                // step 3: add quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity; 
+                // step 4: add the added product to the saved cart
+                savedCart.push(addedProduct);
+            }
+           
+            console.log(addedProduct);
+        }
+        // step 5: set the cart
+        setCart(savedCart);
+    },[products])
 
     const handleAddToCart = (product) =>{
         // cart.push(product) ata kaj korbe na react a push korte hole protek bar new array banate hobe
        const newCart = [...cart, product];
        setCart(newCart);
-       addToDb(product.id);
-       
+       addToDb(product.id); 
     }
+
 
     return (
         <div className='shop-container'>
